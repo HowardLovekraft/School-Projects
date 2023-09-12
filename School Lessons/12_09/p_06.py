6343cc432f6dbee471980b22e4a7d25280e2c144
HowardLovekraft <117375241+HowardLovekraft@users.noreply.github.com>
2023-09-12 21:59:23 +0500

    Create p_06.py

---
+++ School Lessons/12_09/p_06.py
@@ -0,0 +1,11 @@
+for n in range(1000, 10000):
+    num = str(n)
+    s1 = int(num[0]) + int(num[1])
+    s2 = int(num[2]) + int(num[3])
+    if s1 > s2:
+        s3 = str(s1) + str(s2)
+    else:
+        s3 = str(s2) + str(s1)
+    if s3 == "1311":
+        print(num)
+        break
